e25d25d10e688408112481577f1ac7d6a4607335
Ilham Dean Abdillah <deanabdillah@gmail.com>
2025-03-10 18:56:28 +0700

    Update home endpoint message for clarity

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ app.include_router(product_route.router)
 
 @app.get("/")
 def home():
-    return {"message": "Welcome to FastAPI CRUD Product test"}
+    return {"message": "Welcome to FastAPI CRUD Product test2"}
