24aaf6ac73c049ce56085f23ea921cb455344dfe
Ilham Dean Abdillah <deanabdillah@gmail.com>
2025-03-10 17:25:18 +0700

    Update home endpoint message for testing purposes

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(product_route.router)
 
+
 @app.get("/")
 def home():
-    return {"message": "Welcome to FastAPI CRUD Product"}
+    return {"message": "Welcome to FastAPI CRUD Product test"}
